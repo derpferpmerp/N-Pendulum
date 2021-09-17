47f2ea19af4890149b71fc5a9849a884c9a4ebe6
derpferpmerp <cmfleming@eths202.org>
2021-09-17 01:12:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#Resources I Used
+1. [2017-Fall-ME501-02-ODE-Part2.pdf](http://volkov.eng.ua.edu/ME501/2017-Fall-ME501-02-ODE-Part2.pdf) <br>
+2. [SecondOrder.pdf](http://people.uncw.edu/hermanr/mat361/Simulink/SecondOrder.pdf) <br>
+3. [RR_PHY235W_TermPaper.pdf](https://rrubenza.github.io/project/p235_npendulum/RR_PHY235W_TermPaper.pdf) <br>
+4. [Taylor2D.pdf](https://personal.math.ubc.ca/~feldman/m226/taylor2d.pdf) <br>
+5. [Pendulum_(mathematics)](https://en.wikipedia.org/wiki/Pendulum_(mathematics)) <br>
+
+##Formulae
+___
+<figure>
+    <img src="https://latex.codecogs.com/png.image?\dpi{500}\bg_black\ddot{x_k}=g\left[\left(n-k\right)\left(\frac{c_1-x_k}{a}-\frac{x_k-x_{k-1}}{a}\right)-\frac{x_k-x_{k-1}}{a}\right]" alt="None"/>
+    <figcaption>Solution for the Second Derivative of Position in regard to Time</figcaption>
+</figure>
+
---
+++ main.py
@@ -0,0 +1,187 @@
+import json
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from math import *
+import PySimpleGUI as sg
+from scipy import interpolate
+
+sg.theme('DarkTeal12')
+
+plt.style.use("dark_background")
+
+GRAV = 9.81
+
+NSEG = 10
+ARMLEN = 2
+XLIST = []
+YLIST = []
+
+
+def generateColor(bounds=None, amount=3, PLACES=2):
+	if bounds is None:
+		bounds = [0, 255]
+	outlist = []
+	for x in range(amount):
+		outlist.append(random.randrange(bounds[0], bounds[1]))
+
+	return tuple(list(map(lambda x: round(x / bounds[1], PLACES), outlist)))
+
+
+
+
+def trySet(lst, ind, fval=0):
+	try:
+		i = lst[ind]
+	except:
+		return fval
+	return lst[ind]
+
+
+
+
+def smooth(mx, mn, xl, yl, c="blue"):
+	x_new = np.linspace(mn, mx, 1000)
+	a_BSpline = interpolate.make_interp_spline(xl, yl)
+	y_new = a_BSpline(x_new)
+	return x_new, y_new
+
+
+def run(xl=None, al=None):
+	if al is None:
+		al = ARMLEN
+	if xl is None:
+		xl = XLIST
+	OUT = []
+	for k in range(1, len(xl)):
+		p0 = GRAV
+
+		kp1 = trySet(xl, k + 1)
+		kp0 = trySet(xl, k)
+		km1 = trySet(xl, k - 1)
+
+		p21 = (NSEG - k)
+		p221 = ((kp1 - kp0) / al)
+		p222 = ((kp0 - km1) / al)
+		p22 = p221 - p222
+		p2 = p21 * p22
+
+		p3 = ((kp0 - km1) / al)
+
+		p1 = p2 - p3
+
+		OUT.append(p0 * p1)
+	return OUT
+
+
+def graph(amt, NSEG=NSEG, ARMLEN=2):
+	XLIST = []
+	YLIST = []
+	for x in range(NSEG):
+		if not XLIST:
+			gx = 0
+			yc = ARMLEN * NSEG
+			XLIST.append(gx)
+			YLIST.append(yc)
+		else:
+			prev = XLIST[x - 1]
+			genX = random.randrange(-ARMLEN, ARMLEN)
+			yc = ((NSEG * ARMLEN) - (ARMLEN * x))
+			XLIST.append(genX)
+			YLIST.append(yc)
+	OUT = [0] + run(xl=XLIST, al=ARMLEN)
+	fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+	ax2.plot(XLIST, YLIST, color="red", label="Iteration 0")
+	for x in range(NSEG):
+		g = ARMLEN * x
+		xlPLT2 = []
+		ylPLT2 = []
+		for h in np.linspace(min(XLIST), max(XLIST), 1000):
+			xlPLT2.append(h)
+			ylPLT2.append(g)
+		ax2.plot(
+			xlPLT2,
+			ylPLT2,
+			linestyle="dashed",
+			alpha=0.4
+		)
+		OUT = max(XLIST) * (np.array(OUT) / max(OUT))
+		ax2.plot(OUT, YLIST)
+
+	dct = dict([(
+		x,
+		{
+			"ITM": [],
+			"CLR": generateColor()
+		}) for x in range(amt)
+	])
+	labels = []
+	for x in range(amt):
+		rOut = [0] + run(xl=OUT)
+		OUT = max(XLIST) * (np.array(rOut) / max(rOut))
+		for g in range(NSEG - 1):
+			try:
+				dct[g]["ITM"].append(OUT[x])
+			except:
+				continue
+		if x + 1 not in labels:
+			ax2.plot(OUT, YLIST, color=generateColor(), label=f"Iteration {x + 1}")
+			labels.append(x + 1)
+	print(json.dumps(dct, indent=4))
+	lk = list(dct.values())
+	for v in lk:
+		rx, ry = smooth(0, amt - 1, list(range(amt - 1)), v["ITM"][0:-1])
+		if lk.index(v) == len(lk) - 1:
+			ax1.plot(
+				rx,
+				ry,
+				color=v["CLR"],
+				lw=3,
+				alpha=0.8,
+				label="Smoothed Line"
+			)
+			ax1.plot(
+				list(range(amt - 1)),
+				v["ITM"][0:-1],
+				color=(220 / 255, 220 / 255, 220 / 255),
+				lw=4,
+				alpha=0.3,
+				linestyle="dashed",
+				label="Original Line"
+			)
+	for v in lk:
+		try:
+			ax1.scatter(
+				list(range(amt - 1))[lk.index(v)],
+				v["ITM"][0:-1][lk.index(v)],
+				label=f"Step {lk.index(v)}",
+				color=v["CLR"],
+				alpha=1,
+				zorder=2.5
+			)
+		except:
+			continue
+
+	ax1.legend()
+	ax1.set_xlabel("$\\Delta{}\\tau$", fontsize=20, color="white")
+	ax1.text(
+		1 / 3,
+		0.95,
+		"$\\ddot{x_k}=g\\left[\\left(n-k\\"
+		"right)\\left(\\frac{c_1-x_k}{a}-\\frac{"
+		"x_k-x_{k-1}}{a}\\right)-\\frac{x_k-x_{k-1}}"
+		"{a}\\right]$",
+		transform=ax1.transAxes,
+		fontsize=14,
+		bbox=dict(
+			boxstyle='round',
+			facecolor='black',
+			alpha=0.5
+		),
+		horizontalalignment="center"
+	)
+	ax1.set_ylabel("$dx$")
+	ax2.legend()
+
+graph(7,NSEG=20,ARMLEN=7)
+plt.show()
